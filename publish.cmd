d93776ad859e81c008b4af19676a829544074e0b
Bart Van Geel <bart.van.geel@bvgitservices.be>
2019-09-30 16:15:18 +0200

    Initial commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=climateemergencyenglish-bot-Web-Deploy.pubxml -p:Password=ylgchxmAKlbyzr2X6JX3hihwiqmQ3znuvCkN7QSe8htRarGlZ6ob6rx0ctW8
+
